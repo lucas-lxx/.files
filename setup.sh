38515a754a78d9a6a218f6895a8d1c58955953f1
Lucas Almeida <lucas.lxx@alu.ufc.br>
2025-01-14 14:18:08 -0300

    adds a simple script to setup the links of the dotfiles

---
+++ setup.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+declare -A default_path
+declare -A dotfile_path
+
+default_path["bashrc"]="$HOME/.bashrc"
+default_path["zshrc"]="$HOME/.zshrc"
+default_path["nvim"]="$HOME/.config/nvim"
+default_path["tmux"]="$HOME/.tmux.conf"
+
+dotfile_path["bashrc"]="$PWD/bashrc/.bashrc"
+dotfile_path["zshrc"]="$PWD/zshrc/.zshrc"
+dotfile_path["nvim"]="$PWD/nvim/nvim-lucas-lxx/"
+dotfile_path["tmux"]="$PWD/tmux/.tmux.conf"
+
+for i in *; do
+	z="${default_path["$i"]}"
+	# -z checks if the variable is empty
+	if [ -z "$z" ]; then
+		continue
+	fi
+	# -e checks if the file exists
+	if [ -e "$z" ]; then
+		while true; do
+			read -n 1 -p "do you want to overwrite the dotfile, '$i'? (y/n):" ans
+			echo -e
+			case "$ans" in
+				[yY]) # If yes
+					echo "overwriting $z"
+					rm "$z"
+					ln -s "${dotfile_path["$i"]}" "${default_path["$i"]}"
+					break
+					;;
+				[nN]) # If no
+					echo "keeping $z"
+					break
+					;;
+				*) # Invalid input
+					echo "Please answear y or n"
+					;;
+			esac
+		done
+		continue
+	fi
+	# echo "${dotfile_path["$i"]}"
+	# echo "${default_path["$i"]}"
+	# echo "======================"
+	ln -s "${dotfile_path["$i"]}" "${default_path["$i"]}"
+done
